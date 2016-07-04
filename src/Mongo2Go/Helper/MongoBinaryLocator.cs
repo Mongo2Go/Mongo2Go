643908a16e0a22d0a6b17cc51b48806584d36f9d
Kristofer Linnestjerna <kristofer.linnestjerna@netclean.com>
2016-07-04 13:42:19 +0200

    Support for optional passing of mongo-bin location

---
+++ src/Mongo2Go/Helper/MongoBinaryLocator.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Mongo2Go.Helper
+{
+
+    public class MongoBinaryLocator : IMongoBinaryLocator
+    {
+        private string nugetPrefix = @"packages\Mongo2Go*";
+        private string searchPattern = "";
+
+        public MongoBinaryLocator (string binSearchPattern)
+        {
+            if (string.IsNullOrEmpty (binSearchPattern)) {
+                throw new ArgumentException ("Missing the search pattern for finding the mongoDb binaries.", "binSearchPattern");
+            }
+
+            searchPattern = binSearchPattern;
+        }
+
+        public string ResolveBinariesDirectory ()
+        {
+            // 1st: path when installed via nuget
+            // 2nd: path when started from solution
+            string binariesFolder = FolderSearch.CurrentExecutingDirectory ().FindFolderUpwards (System.IO.Path.Combine (nugetPrefix, searchPattern)) ??
+                                    FolderSearch.CurrentExecutingDirectory ().FindFolderUpwards (searchPattern);
+
+            if (binariesFolder == null) {
+                throw new MonogDbBinariesNotFoundException (string.Format (
+                    "Could not find Mongo binaries using {0}. We walk up the directories {1} levels from {2}",
+                    searchPattern,
+                    FolderSearch.MaxLevelOfRecursion,
+                    FolderSearch.CurrentExecutingDirectory ()));
+            }
+            return binariesFolder;
+        }
+    }
+}
