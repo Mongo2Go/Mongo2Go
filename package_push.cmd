75de4a6ec058ed553dcf0fe56b723d55198c8aa6
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2016-12-20 10:10:20 +0100

    Mongo2Go 2.0.0-alpha1 with support for .NET Standard 1.6 only, which closes #25
    FYI @bannerflow-hassaan @ash23

--- package_push.cmd
+++ package_push.cmd
@@ -1,5 +1,14 @@
 @echo off
 
-nuget push *.nupkg -Timeout 1800
+IF %1.==. GOTO NoParameter
 
+nuget.exe setApiKey %1
+nuget push *.nupkg -Timeout 1800 -Source https://www.nuget.org/api/v2/package
+goto end
+
+:NoParameter
+echo *** ERROR!
+echo *** Please privide the API key from nuget.org like this: package_push [key]
+
+:end
 pause
