Mongo2Go/Mongo2Go

commit 1370889345cfa1530b9aea796cf6048b6baaf4e9

Remove MongoDbDefaults.DataDirectory and don't hardcode backslashes as path separators

src/Mongo2Go/MongoDbRunner.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
 using Mongo2Go.Helper;
 
@@ -30,7 +31,7 @@ namespace Mongo2Go
         /// On dispose: kills them and deletes their data directory
         /// </summary>
         /// <remarks>Should be used for integration tests</remarks>
-        public static MongoDbRunner Start(string dataDirectory = MongoDbDefaults.DataDirectory, string searchPatternOverride = null)
+        public static MongoDbRunner Start(string dataDirectory = null, string searchPatternOverride = null)
         {
             dataDirectory += Guid.NewGuid().ToString().Replace("-", "").Substring(0, 20);
 
@@ -39,7 +40,7 @@ namespace Mongo2Go
 
         public static MongoDbRunner StartUnitTest(IPortPool portPool, IFileSystem fileSystem, IMongoDbProcessStarter processStarter, IMongoBinaryLocator mongoBin)
         {
-            return new MongoDbRunner(portPool, fileSystem, processStarter, mongoBin, MongoDbDefaults.DataDirectory);
+            return new MongoDbRunner(portPool, fileSystem, processStarter, mongoBin);
         }
 
         /// <summary>
@@ -49,14 +50,14 @@ namespace Mongo2Go
         /// Should be used for local debugging only
         /// WARNING: one single instance on one single machine is not a suitable setup for productive environments!!!
         /// </remarks>
-        public static MongoDbRunner StartForDebugging(string dataDirectory = MongoDbDefaults.DataDirectory, string searchPatternOverride = null)
+        public static MongoDbRunner StartForDebugging(string dataDirectory = null, string searchPatternOverride = null)
         {
             return new MongoDbRunner(new ProcessWatcher(), new PortWatcher(), new FileSystem(), new MongoDbProcessStarter(), new MongoBinaryLocator(searchPatternOverride), dataDirectory);
         }
 
         public static MongoDbRunner StartForDebuggingUnitTest(IProcessWatcher processWatcher, IPortWatcher portWatcher, IFileSystem fileSystem, IMongoDbProcessStarter processStarter, IMongoBinaryLocator mongoBin)
         {
-            return new MongoDbRunner(processWatcher, portWatcher, fileSystem, processStarter, mongoBin, MongoDbDefaults.DataDirectory);
+            return new MongoDbRunner(processWatcher, portWatcher, fileSystem, processStarter, mongoBin, null);
         }
 
         /// <summary>
@@ -80,7 +81,7 @@ namespace Mongo2Go
         /// <summary>
         /// usage: local debugging
         /// </summary>
-        private MongoDbRunner(IProcessWatcher processWatcher, IPortWatcher portWatcher, IFileSystem fileSystem, IMongoDbProcessStarter processStarter, IMongoBinaryLocator mongoBin, string dataDirectory)
+        private MongoDbRunner(IProcessWatcher processWatcher, IPortWatcher portWatcher, IFileSystem fileSystem, IMongoDbProcessStarter processStarter, IMongoBinaryLocator mongoBin, string dataDirectory = null)
         {
             _fileSystem = fileSystem;
             _port = MongoDbDefaults.DefaultPort;
@@ -101,6 +102,10 @@ namespace Mongo2Go
                 throw new MongoDbPortAlreadyTakenException("MongoDB can't be started. The TCP port {0} is already taken.".Formatted(_port));
             }
 
+            if (dataDirectory == null) {
+                dataDirectory = CreateTemporaryDataDirectory();
+            }
+
             _fileSystem.CreateFolder(dataDirectory);
             _fileSystem.DeleteFile(@"{0}{1}{2}".Formatted(dataDirectory, System.IO.Path.DirectorySeparatorChar.ToString(), MongoDbDefaults.Lockfile));
             _mongoDbProcess = processStarter.Start(_mongoBin.Directory, dataDirectory, _port, true);
@@ -111,11 +116,15 @@ namespace Mongo2Go
         /// <summary>
         /// usage: integration tests
         /// </summary>
-        private MongoDbRunner(IPortPool portPool, IFileSystem fileSystem, IMongoDbProcessStarter processStarter, IMongoBinaryLocator mongoBin, string dataDirectory)
+        private MongoDbRunner(IPortPool portPool, IFileSystem fileSystem, IMongoDbProcessStarter processStarter, IMongoBinaryLocator mongoBin, string dataDirectory = null)
         {
             _fileSystem = fileSystem;
             _port = portPool.GetNextOpenPort();
             _mongoBin = mongoBin;
+
+            if (dataDirectory == null) {
+                dataDirectory = CreateTemporaryDataDirectory();
+            }
 
             MakeMongoBinarysExecutable();
 
@@ -138,6 +147,12 @@ namespace Mongo2Go
                 _fileSystem.MakeFileExecutable(System.IO.Path.Combine(_mongoBin.Directory, MongoDbDefaults.MongoExportExecutable));
                 _fileSystem.MakeFileExecutable(System.IO.Path.Combine(_mongoBin.Directory, MongoDbDefaults.MongoImportExecutable));
             }
+        }
+
+        private string CreateTemporaryDataDirectory() {
+            var path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(path);
+            return path;
         }
     }
 }

src/Mongo2Go/Helper/MongoImportExport.cs
@@ -17,7 +17,7 @@ namespace Mongo2Go.Helper
                 throw new FileNotFoundException("File not found", finalPath);
             }
             
-            string fileName = @"{0}\{1}".Formatted(binariesDirectory, MongoDbDefaults.MongoImportExecutable);
+            string fileName = Path.Combine("{0}", "{1}").Formatted(binariesDirectory, MongoDbDefaults.MongoImportExecutable);
             string arguments = @"--host localhost --port {0} --db {1} --collection {2} --file ""{3}""".Formatted(port, database, collection, finalPath);
             if (drop) { arguments += " --drop"; }
 
@@ -34,7 +34,7 @@ namespace Mongo2Go.Helper
         {
             string finalPath = FolderSearch.FinalizePath(outputFile);
 
-            string fileName = @"{0}\{1}".Formatted(binariesDirectory, MongoDbDefaults.MongoExportExecutable);
+            string fileName = Path.Combine("{0}", "{1}").Formatted(binariesDirectory, MongoDbDefaults.MongoExportExecutable);
             string arguments = @"--host localhost --port {0} --db {1} --collection {2} --out ""{3}""".Formatted(port, database, collection, finalPath);
 
             Process process = ProcessControl.ProcessFactory(fileName, arguments);

src/Mongo2Go/MongoDbDefaults.cs
@@ -15,8 +15,6 @@
         // but we don't want to get in trouble with productive systems
         public const int TestStartPort = 27018;
 
-        public const string DataDirectory = @"C:\data\db";
-
         public const string Lockfile = "mongod.lock";
     }
 }

src/Mongo2GoTests/Runner/RunnerTests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.IO;
+using FluentAssertions;
 using Machine.Specifications;
 using Mongo2Go;
 using Mongo2Go.Helper;
@@ -18,8 +20,8 @@ namespace Mongo2GoTests.Runner
         static Mock<IMongoDbProcessStarter> processStarterMock;
         static Mock<IMongoBinaryLocator> binaryLocatorMock;
 
-        static readonly string exptectedDataDirectory = "{0}_{1}".Formatted(MongoDbDefaults.DataDirectory, MongoDbDefaults.TestStartPort + 1);
-        static readonly string exptectedLogfile = @"{0}_{1}\{2}".Formatted(MongoDbDefaults.DataDirectory, MongoDbDefaults.TestStartPort + 1, MongoDbDefaults.Lockfile);
+        static string exptectedDataDirectory;
+        static string exptectedLogfile;
         static readonly string exptectedConnectString = "mongodb://localhost:{0}/".Formatted(MongoDbDefaults.TestStartPort + 1);
 
         Establish context = () =>
@@ -28,6 +30,11 @@ namespace Mongo2GoTests.Runner
             portPoolMock.Setup(m => m.GetNextOpenPort()).Returns(MongoDbDefaults.TestStartPort + 1);
 
             fileSystemMock = new Mock<IFileSystem>();
+            fileSystemMock.Setup(m => m.CreateFolder(Moq.It.IsAny<string>())).Callback<string>(s => 
+            {
+                exptectedDataDirectory = s;
+                exptectedLogfile = Path.Combine(exptectedDataDirectory, MongoDbDefaults.Lockfile);
+            });
             
             var processMock = new Mock<IMongoDbProcess>();
 
@@ -35,12 +42,13 @@ namespace Mongo2GoTests.Runner
             processStarterMock.Setup(m => m.Start(Moq.It.IsAny<string>(), Moq.It.IsAny<string>(), Moq.It.IsAny<int>())).Returns(processMock.Object);
 
             binaryLocatorMock = new Mock<IMongoBinaryLocator> ();
-
+            binaryLocatorMock.Setup(m => m.Directory).Returns(string.Empty);
         };
 
         Because of = () => runner = MongoDbRunner.StartUnitTest(portPoolMock.Object, fileSystemMock.Object, processStarterMock.Object, binaryLocatorMock.Object);
 
-        It should_create_the_data_directory             = () => fileSystemMock.Verify(x => x.CreateFolder(exptectedDataDirectory), Times.Exactly(1));
+        string dataDirectory;
+        It should_create_the_data_directory             = () => fileSystemMock.Verify(x => x.CreateFolder(Moq.It.Is<string>(s => s.StartsWith(Path.GetTempPath()))), Times.Exactly(1));
         It should_delete_old_lock_file                  = () => fileSystemMock.Verify(x => x.DeleteFile(exptectedLogfile), Times.Exactly(1));
 
         It should_start_the_process                     = () => processStarterMock.Verify(x => x.Start(Moq.It.IsAny<string>(), Moq.It.IsAny<string>(), Moq.It.IsAny<int>()), Times.Exactly(1));
@@ -59,7 +67,8 @@ namespace Mongo2GoTests.Runner
         static Mock<IMongoDbProcessStarter> processStarterMock;
         static Mock<IMongoBinaryLocator> binaryLocatorMock;
 
-        static readonly string exptectedLogfile = @"{0}\{1}".Formatted(MongoDbDefaults.DataDirectory, MongoDbDefaults.Lockfile);
+        static string exptectedDataDirectory;
+        static string exptectedLogfile;
 
         Establish context = () =>
         {
@@ -70,22 +79,28 @@ namespace Mongo2GoTests.Runner
             portWatcherMock.Setup(m => m.IsPortAvailable(Moq.It.IsAny<int>())).Returns(true);
 
             fileSystemMock = new Mock<IFileSystem>();
+            fileSystemMock.Setup(m => m.CreateFolder(Moq.It.IsAny<string>())).Callback<string>(s => 
+            {
+                exptectedDataDirectory = s;
+                exptectedLogfile = Path.Combine(exptectedDataDirectory, MongoDbDefaults.Lockfile);
+            });
 
             var processMock = new Mock<IMongoDbProcess>();
             processStarterMock = new Mock<IMongoDbProcessStarter>();
-            processStarterMock.Setup(m => m.Start(Moq.It.IsAny<string>(), MongoDbDefaults.DataDirectory, MongoDbDefaults.DefaultPort, true)).Returns(processMock.Object);
+            processStarterMock.Setup(m => m.Start(Moq.It.IsAny<string>(), exptectedDataDirectory, MongoDbDefaults.DefaultPort, true)).Returns(processMock.Object);
 
             binaryLocatorMock = new Mock<IMongoBinaryLocator> ();
+            binaryLocatorMock.Setup(m => m.Directory).Returns(string.Empty);
         };
 
         Because of = () => runner = MongoDbRunner.StartForDebuggingUnitTest(processWatcherMock.Object, portWatcherMock.Object, fileSystemMock.Object, processStarterMock.Object, binaryLocatorMock.Object);
 
         It should_check_for_already_running_process = () => processWatcherMock.Verify(x => x.IsProcessRunning(MongoDbDefaults.ProcessName), Times.Exactly(1));
         It should_check_the_default_port = () => portWatcherMock.Verify(x => x.IsPortAvailable(MongoDbDefaults.DefaultPort), Times.Exactly(1));
-        It should_create_the_data_directory = () => fileSystemMock.Verify(x => x.CreateFolder(MongoDbDefaults.DataDirectory), Times.Exactly(1));
+        It should_create_the_data_directory = () => fileSystemMock.Verify(x => x.CreateFolder(Moq.It.Is<string>(s => s.StartsWith(Path.GetTempPath()))), Times.Exactly(1));
         It should_delete_old_lock_file = () => fileSystemMock.Verify(x => x.DeleteFile(exptectedLogfile), Times.Exactly(1));
 		It should_return_an_instance_with_state_running = () => runner.State.Should().Be(State.Running);
-        It should_start_the_process_without_kill = () => processStarterMock.Verify(x => x.Start(Moq.It.IsAny<string>(), MongoDbDefaults.DataDirectory, MongoDbDefaults.DefaultPort, true), Times.Exactly(1));
+        It should_start_the_process_without_kill = () => processStarterMock.Verify(x => x.Start(Moq.It.IsAny<string>(), exptectedDataDirectory, MongoDbDefaults.DefaultPort, true), Times.Exactly(1));
     }
 }
 // ReSharper restore UnusedMember.Local

commit 8e437998e798b55969e9e881405a62854f4fdb3e

fixes regression introduced by #42
FYI @perliedman

src/Mongo2Go/MongoDbRunner.cs
@@ -33,6 +33,11 @@ namespace Mongo2Go
         /// <remarks>Should be used for integration tests</remarks>
         public static MongoDbRunner Start(string dataDirectory = null, string searchPatternOverride = null)
         {
+            if (dataDirectory == null) {
+                dataDirectory = CreateTemporaryDataDirectory();
+            }
+
+            // this is required to support multiple instances to run in parallel
             dataDirectory += Guid.NewGuid().ToString().Replace("-", "").Substring(0, 20);
 
             return new MongoDbRunner(
@@ -194,7 +199,7 @@ namespace Mongo2Go
             }
         }
 
-        private string CreateTemporaryDataDirectory() {
+        private static string CreateTemporaryDataDirectory() {
             var path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
             Directory.CreateDirectory(path);
             return path;