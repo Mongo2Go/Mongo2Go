97b793960ea486a52874926d6499fc0c2c27c11a
Mahi Satyanarayana <msatya@appriver.com>
2018-09-26 17:09:27 -0500

    Added tests to demonstrate transactions

---
+++ src/Mongo2GoTests/Runner/MongoTransactionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mongo2Go;
+using MongoDB.Driver;
+
+namespace Mongo2GoTests.Runner
+{
+    public class MongoTransactionTest
+    {
+        internal static MongoDbRunner _runner;
+        internal static IMongoCollection<TestDocument> _mainCollection;
+        internal static IMongoCollection<TestDocument> _dependentCollection;
+        internal static string _databaseName = "TransactionTest";
+        internal static string _mainCollectionName = "MainCollection";
+        internal static string _dependentCollectionName = "DependentCollection";
+        internal static IMongoDatabase database;
+        internal static IMongoClient client;
+        internal static void CreateConnection()
+        {
+            _runner = MongoDbRunner.Start(singleNodeReplSet: true);
+
+            client = new MongoClient(_runner.ConnectionString);
+            database = client.GetDatabase(_databaseName);
+            _mainCollection = database.GetCollection<TestDocument>(_mainCollectionName);
+            _dependentCollection = database.GetCollection<TestDocument>(_dependentCollectionName);
+        }
+    }
+}
---
+++ src/Mongo2GoTests/Runner/RunnerTransactionTests.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Machine.Specifications;
+using MongoDB.Driver;
+
+namespace Mongo2GoTests.Runner
+{
+    [Subject("Runner Transaction Test")]
+    public class when_transaction_completes : MongoTransactionTest
+    {
+        private static TestDocument mainDocument;
+        private static TestDocument dependentDocument;
+        Establish context = () =>
+
+        {
+            CreateConnection();
+            database.DropCollection(_mainCollectionName);
+            database.DropCollection(_dependentCollectionName);
+            _mainCollection.InsertOne(TestDocument.DummyData2());
+            _dependentCollection.InsertOne(TestDocument.DummyData2());
+        };
+
+        private Because of = () =>
+        {
+            var filter = Builders<TestDocument>.Filter.Where(x => x.IntTest == 23);
+            var update = Builders<TestDocument>.Update.Inc(i => i.IntTest, 10);
+
+            using (var sessionHandle = client.StartSession())
+            {
+                try
+                {
+                    var i = 0;
+                    while (i < 10)
+                    {
+                        try
+                        {
+                            i++;
+                            sessionHandle.StartTransaction(new TransactionOptions(
+                                readConcern: ReadConcern.Local,
+                                writeConcern: WriteConcern.W1)); 
+                            try
+                            {
+                                var first = _mainCollection.UpdateOne(sessionHandle, filter, update);
+                                var second = _dependentCollection.UpdateOne(sessionHandle, filter, update);
+                            }
+                            catch (Exception e)
+                            {
+                                sessionHandle.AbortTransaction();
+                                throw;
+                            }
+
+                            var j = 0;
+                            while (j < 10)
+                            {
+                                try
+                                {
+                                    j++;
+                                    sessionHandle.CommitTransaction();
+                                    break;
+                                }
+                                catch (MongoException e)
+                                {
+                                    if (e.HasErrorLabel("UnknownTransactionCommitResult"))
+                                        continue;
+                                    throw;
+                                }
+                            }
+                            break;
+                        }
+                        catch (MongoException e)
+                        {
+                            if (e.HasErrorLabel("TransientTransactionError"))
+                                continue;
+                            throw;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    
+                }
+            }
+
+             mainDocument = _mainCollection.FindSync(Builders<TestDocument>.Filter.Empty).FirstOrDefault();
+             dependentDocument = _dependentCollection.FindSync(Builders<TestDocument>.Filter.Empty).FirstOrDefault();
+        };
+        
+        It main_should_be_33 = () => mainDocument.IntTest.Should().Be(33);
+        It dependent_should_be_33 = () => dependentDocument.IntTest.Should().Be(33);
+        Cleanup cleanup = () => _runner.Dispose();
+    }
+
+
+    [Subject("Runner Transaction Test")]
+    public class when_transaction_is_aborted_before_commit : MongoTransactionTest
+    {
+        private static TestDocument mainDocument;
+        private static TestDocument dependentDocument;
+        Establish context = () =>
+
+        {
+            CreateConnection();
+            database.DropCollection(_mainCollectionName);
+            database.DropCollection(_dependentCollectionName);
+            _mainCollection.InsertOne(TestDocument.DummyData2());
+            _dependentCollection.InsertOne(TestDocument.DummyData2());
+
+        };
+
+        private Because of = () =>
+        {
+            var filter = Builders<TestDocument>.Filter.Where(x => x.IntTest == 23);
+            var update = Builders<TestDocument>.Update.Inc(i => i.IntTest, 10);
+          
+            using (var sessionHandle = client.StartSession())
+            {
+                try
+                {
+                    var i = 0;
+                    while (i < 2)
+                    {
+                        try
+                        {
+                            i++;
+                            sessionHandle.StartTransaction(new TransactionOptions(
+                                readConcern: ReadConcern.Local,
+                                writeConcern: WriteConcern.W1));
+                            try
+                            {
+                                var first = _mainCollection.UpdateOne(sessionHandle, filter, update);
+                                var second = _dependentCollection.UpdateOne(sessionHandle, filter, update);
+                            }
+                            catch (Exception e)
+                            {
+                                sessionHandle.AbortTransaction();
+                                throw;
+                            }
+
+                            //Throw exception and do not commit
+                            throw new ApplicationException();
+                        }
+                        catch (MongoException e)
+                        {
+                            if (e.HasErrorLabel("TransientTransactionError"))
+                                continue;
+                            throw;
+                        }
+
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+
+            mainDocument = _mainCollection.FindSync(Builders<TestDocument>.Filter.Empty).FirstOrDefault();
+            dependentDocument = _dependentCollection.FindSync(Builders<TestDocument>.Filter.Empty).FirstOrDefault();
+        };
+
+        It main_should_be_still_23_after_aborting = () => mainDocument.IntTest.Should().Be(23);
+        It dependent_should_be_still_23_after_aborting = () => dependentDocument.IntTest.Should().Be(23);
+
+        Cleanup cleanup = () => _runner.Dispose();
+    }
+}
