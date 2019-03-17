6250f6a4449a43f1c204cb7720969d17579141f9
José Mira <jose.rui.mira@gmail.com>
2019-03-17 14:22:27 +0000

    add mongod additional arguments cleanup logic
    make sure any additional argument passed in does not clash with already defined, by Mongo2Go, mongod arguments.

---
+++ src/Mongo2Go/Helper/MongodArguments.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mongo2Go.Helper
+{
+    public static class MongodArguments
+    {
+        private const string ArgumentSeparator = "--";
+        private const string Space = " ";
+
+        /// <summary>
+        /// This method will cleanup the additional mongod arguments to make sure the Mongo2Go arguments are not overriden
+        /// </summary>
+        /// <param name="existingMongodArguments">Mongo2Go defined mongod arguments</param>
+        /// <param name="additionalMongodArguments">Additional custom mongod arguments</param>
+        /// <returns>Additional mongod arguments excluding the Mogo2Go defined mongod arguments</returns>
+        public static string GetValidAdditionalArguments(string existingMongodArguments, string additionalMongodArguments)
+        {
+            if (string.IsNullOrWhiteSpace(additionalMongodArguments))
+            {
+                return string.Empty;
+            }
+
+            var existingMongodArgumentArray = existingMongodArguments.Trim().Split(new[] { ArgumentSeparator }, StringSplitOptions.RemoveEmptyEntries);
+
+            var existingMongodArgumentOptions = new List<string>();
+            for (var i = 0; i < existingMongodArgumentArray.Length; i++)
+            {
+                var argumentOptionSplit = existingMongodArgumentArray[i].Split(' ');
+
+                if (argumentOptionSplit.Length == 0
+                    || string.IsNullOrWhiteSpace(argumentOptionSplit[0].Trim()))
+                {
+                    continue;
+                }
+
+                existingMongodArgumentOptions.Add(argumentOptionSplit[0].Trim());
+            }
+
+            var additionalMongodArgumentArray = additionalMongodArguments.Trim().Split(new[] { ArgumentSeparator }, StringSplitOptions.RemoveEmptyEntries);
+
+            var validAdditionalMongodArguments = new List<string>();
+            for (var i = 0; i < additionalMongodArgumentArray.Length; i++)
+            {
+                var additionalArgument = additionalMongodArgumentArray[i].Trim();
+                var argumentOptionSplit = additionalArgument.Split(' ');
+
+                if (argumentOptionSplit.Length == 0
+                    || string.IsNullOrWhiteSpace(argumentOptionSplit[0].Trim())
+                    || existingMongodArgumentOptions.Contains(argumentOptionSplit[0].Trim()))
+                {
+                    continue;
+                }
+
+                validAdditionalMongodArguments.Add(ArgumentSeparator + additionalArgument);
+            }
+
+            return validAdditionalMongodArguments.Count == 0
+                ? string.Empty
+                : Space + string.Join(" ", validAdditionalMongodArguments);
+        }
+    }
+}
---
+++ src/Mongo2GoTests/MongoDbAdditionalArgumentsTests.cs
@@ -0,0 +1,74 @@
+﻿using FluentAssertions;
+using Machine.Specifications;
+using Mongo2Go.Helper;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Local
+namespace Mongo2GoTests
+{
+    [Subject(typeof(MongodArguments))]
+    public class when_null_additional_arguments_return_empty_string
+    {
+        private static string validAdditionalArguments;
+
+        Because of = () => validAdditionalArguments = MongodArguments.GetValidAdditionalArguments(string.Empty, null);
+        It should_be_empty_string = () => validAdditionalArguments.Should().BeEmpty();
+    }
+
+    [Subject(typeof(MongodArguments))]
+    public class when_no_additional_arguments_return_empty_string
+    {
+        private static string validAdditionalArguments;
+
+        Because of = () => validAdditionalArguments = MongodArguments.GetValidAdditionalArguments(string.Empty, string.Empty);
+        It should_be_empty_string = () => validAdditionalArguments.Should().BeEmpty();
+    }
+
+    [Subject(typeof(MongodArguments))]
+    public class when_additional_arguments_start_with_argument_separator_return_additional_arguments
+    {
+        private static string validAdditionalArguments;
+        private const string additionalArgumentsUnderTest = " --argument_1 under_test --argument_2 under test";
+        private const string expectedAdditionalArguments = " --argument_1 under_test --argument_2 under test";
+
+        Because of = () => validAdditionalArguments = MongodArguments.GetValidAdditionalArguments(string.Empty, additionalArgumentsUnderTest);
+        It should_be_empty_string = () => validAdditionalArguments.Should().Be(expectedAdditionalArguments);
+    }
+
+    [Subject(typeof(MongodArguments))]
+    public class when_additional_arguments_does_not_start_with_argument_separator_return_additional_arguments
+    {
+        private static string validAdditionalArguments;
+        private const string additionalArgumentsUnderTest = "argument_1 under_test --argument_2 under test";
+        private const string expectedAdditionalArguments = " --argument_1 under_test --argument_2 under test";
+
+        Because of = () => validAdditionalArguments = MongodArguments.GetValidAdditionalArguments(string.Empty, additionalArgumentsUnderTest);
+        It should_be_empty_string = () => validAdditionalArguments.Should().Be(expectedAdditionalArguments);
+    }
+
+    [Subject(typeof(MongodArguments))]
+    public class when_existing_arguments_and_additional_arguments_do_not_have_shared_options_return_additional_arguments
+    {
+        private static string validAdditionalArguments;
+        private const string existingArguments = "--existing_argument1 --existing_argument2";
+        private const string additionalArgumentsUnderTest = " --argument_1 under_test --argument_2 under test";
+        private const string expectedAdditionalArguments = " --argument_1 under_test --argument_2 under test";
+
+        Because of = () => validAdditionalArguments = MongodArguments.GetValidAdditionalArguments(existingArguments, additionalArgumentsUnderTest);
+        It should_be_empty_string = () => validAdditionalArguments.Should().Be(expectedAdditionalArguments);
+    }
+
+    [Subject(typeof(MongodArguments))]
+    public class when_existing_arguments_and_additional_arguments_have_shared_options_return_additional_arguments_excluding_common_options
+    {
+        private static string validAdditionalArguments;
+        private const string existingArguments = "--existing_argument1 --existing_argument2";
+        private const string additionalArgumentsUnderTest = " --argument_1 under_test --existing_argument2 argument2_new_value --argument_2 under test";
+        private const string expectedAdditionalArguments = " --argument_1 under_test --argument_2 under test";
+
+        Because of = () => validAdditionalArguments = MongodArguments.GetValidAdditionalArguments(existingArguments, additionalArgumentsUnderTest);
+        It should_be_empty_string = () => validAdditionalArguments.Should().Be(expectedAdditionalArguments);
+    }
+}
+// ReSharper restore UnusedMember.Local
+// ReSharper restore InconsistentNaming
